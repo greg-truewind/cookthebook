8fb5a2c1f558fea4401f92b9e4f808e7538b1542
Greg Konush <greg@trytruewind.com>
2024-10-09 10:50:42 -0700

    add upload page and oauth2 callback

--- src/app/api/oauth2/callback/route.ts
+++ src/app/api/oauth2/callback/route.ts
@@ -1,28 +1,20 @@
-import { NextRequest, NextResponse } from 'next/server';
-import OAuthClient from 'intuit-oauth';
-import { fstat } from 'fs';
+import { type NextRequest, NextResponse } from "next/server"
+import OAuthClient from "intuit-oauth"
+import { redirect } from "next/navigation"
 
 export async function GET(request: NextRequest) {
   const oauthClient = new OAuthClient({
-    clientId: process.env.CLIENT_ID!,
-    clientSecret: process.env.CLIENT_SECRET!,
-    environment: 'sandbox', // or 'production'
+    clientId: process.env.INTUIT_CLIENT_ID!,
+    clientSecret: process.env.INTUIT_SECRET_KEY!,
+    environment: "sandbox", // or 'production'
     redirectUri: process.env.REDIRECT_URI!,
-  });
+  })
 
-  const url = new URL(request.url);
-  const query = url.searchParams.toString();
+  const url = new URL(request.url)
+  const query = url.searchParams.toString()
 
-  try {
-    const parseRedirect = await oauthClient.createToken(query);
+  const token = await oauthClient.createToken(query)
 
-    // Handle token as needed (e.g., store in database, session, etc.)
-    console.log('Access Token:', parseRedirect.getJson());
-
-    return NextResponse.redirect('/');
-  } catch (error) {
-    console.error('Error obtaining access token:', error);
-
-    return NextResponse.json({ error: 'Failed to obtain access token.' }, { status: 500 });
-  }
+  console.log("Access Token:", token)
+  redirect("/upload")
 }
